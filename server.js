e30711d1717880f6707c2eb772efd31606e5fcc1
abedihamid257-afk <abedihamid257@gmail.com>
2026-08-02 22:17:21 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const SECRET_PASS = process.env.BOMBER_PASSWORD || '5879';
 
 app.use(express.static('public'));
 app.use(express.json());
+
+app.get('/api/get-secret', (req, res) => {
+    res.json({ secret: SECRET_PASS });
+});
 
 app.post('/api/check-pass', (req, res) => {
     const { password } = req.body;
